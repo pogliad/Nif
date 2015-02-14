3db0822ce6493deff470adfe6e56d10bda832040
kokhan_s <kokhan_s@ukr.net>
2015-02-14 18:58:39 +0200

    Removed ToListAsync extension.

--- Nif.Extensions/CollectionExtensions.cs
+++ Nif.Extensions/CollectionExtensions.cs
@@ -1,20 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics.Contracts;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Nif.Extensions
 {
     public static class CollectionExtensions
     {
-        public static Task<List<T>> ToListAsync<T>(this IQueryable<T> source)
-        {
-            Contract.Requires<ArgumentNullException>(source != null);
-
-            return Task.Run(() => source.ToList());
-        }
-
         /// <summary>
         /// Fisher-Yates Shuffle
         /// </summary>
